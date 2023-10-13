f9445bb405cac4d488ff0604169e6c49be089981
JEFF-PC\jeff-PC <j22032301@gmail.com>
2023-10-13 21:50:16 +0800

    簡化程式碼判斷

--- src/PaginationContain/PaginationContain.js
+++ src/PaginationContain/PaginationContain.js
@@ -27,12 +27,9 @@ function PaginationContain ({data,setTest,testData,currPage,setCurrPage}) {
                 <button className="list-none text-sm w-[5rem] h-[2rem] bg-slate-800 border border-slate-600 flex items-center justify-center rounded-l-lg hover:bg-slate-600 text-slate-400 hover:text-white font-bold"onClick={() => getData(currPage-1)}>上一頁</button>
                 {data.map((x,y) => (
                     y<data.length/10 ? (
-                        currPage == y+1 ? (
-                            <button className="list-none text-sm w-[2rem] h-[2rem] bg-slate-600 border border-slate-600 flex items-center justify-center text-white" onClick={() => getData(y+1)}>{y+1}</button>
-                        ) : <button className="list-none text-sm w-[2rem] h-[2rem] bg-slate-800 border border-slate-600 flex items-center justify-center hover:bg-slate-600 text-slate-400 hover:text-white" onClick={() => getData(y+1)}>{y+1}</button>
-                        
+                        // ${是原生js插入語法}用``符號來允許插入js並非'
+                        <button className={`list-none text-sm w-[2rem] h-[2rem] bg-slate-800 border border-slate-600 flex items-center justify-center text-white ${currPage == y+1 ? "bg-slate-600 text-white" : "bg-slate-800 text-slate-400"}`}  onClick={() => getData(y+1)}>{y+1}</button>
                     ) : null
-                    
                 ))}
                 <button className="list-none text-sm w-[5rem] h-[2rem] bg-slate-800 border border-slate-600 flex items-center justify-center rounded-r-lg hover:bg-slate-600 text-slate-400 hover:text-white font-bold"onClick={() => getData(currPage+1)}>下一頁</button>
             </div>
